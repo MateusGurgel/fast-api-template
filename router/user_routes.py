34e4e01716d2e72682679dcfd8cd5d356d48ed90
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-04-29 18:36:54 -0300

    feat: "/me route added"

--- router/user_routes.py
+++ router/user_routes.py
@@ -1,14 +1,19 @@
-from fastapi import APIRouter, Depends
-from database import get_db
-from schematics.user_schema import CreateUserSchema
+from fastapi import APIRouter
+from schemas.user_schema import CreateUserSchema, UserListSchema
 from services import user_service
-from sqlalchemy.orm import Session
+from dependencies import current_user
+from dependencies import current_user, database
 
 
-user_router = APIRouter(prefix=("/users"), tags=["users"])
+user_router = APIRouter(prefix=("/users"), tags=["user"])
 
 
 @user_router.post("/", summary="Create a new user")
-async def create_user(user: CreateUserSchema, db: Session = Depends(get_db)):
+async def create_user(user: CreateUserSchema, db: database):
     user = user_service.create_user(user, db)
+    return user
+
+
+@user_router.get("/me", summary="Get current user")
+async def get_current_user(user: current_user) -> UserListSchema:
     return user

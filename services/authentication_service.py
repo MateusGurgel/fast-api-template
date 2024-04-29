09d2794d5ff1db5b84d9548956f9d569d1525ca5
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-04-29 19:32:06 -0300

    feat: using env vars for encryption and jwt tokens

--- services/authentication_service.py
+++ services/authentication_service.py
@@ -9,9 +9,9 @@ from typing import Union
 import os
 
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_SECRET_KEY = os.getenv("ACCESS_TOKEN_SECRET_KEY")
+ACCESS_TOKEN_ENCRYPTION_ALGORITHM = os.getenv("ACCESS_TOKEN_ENCRYPTION_ALGORITHM")
 
 
 def authenticate(username: str, password, db: Session) -> str:
@@ -33,7 +33,7 @@ def try_get_user_username_from_token(token: str) -> str:
 
 
 def get_user_username_from_token(token: str) -> str:
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    payload = jwt.decode(token, ACCESS_TOKEN_SECRET_KEY, algorithms=[ACCESS_TOKEN_ENCRYPTION_ALGORITHM])
     username = payload.get("sub")
 
     if not username:
@@ -50,7 +50,7 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
 
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, ACCESS_TOKEN_SECRET_KEY, algorithm=ACCESS_TOKEN_ENCRYPTION_ALGORITHM)
     return encoded_jwt
 
 
--- services/encryption_service.py
+++ services/encryption_service.py
@@ -1,9 +1,9 @@
 from passlib.context import CryptContext
+import os
 
-SALT = "iB41i26eE4ivjtYB1RMgHm"
 
+SALT = os.getenv("SALT")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)

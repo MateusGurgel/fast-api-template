6f871d72c346bb5e78a3cdfa18bf725f28a1fded
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-04-29 18:34:26 -0300

    feat: App name and description created fix: CORSMiddleware properly added

--- main.py
+++ main.py
@@ -5,15 +5,36 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 
 
-app = FastAPI()
+description = """
+## My FastAPI Template
+"""
+
+app = FastAPI(
+    title="FastAPI Template",
+    description=description,
+    summary="That is my favorite template.",
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+)
+
 
 # CORS
 
+
 origins_string = os.getenv("ALLOWED_HOSTS")
 origins = origins_string.split(",")
-app.add_middleware(CORSMiddleware(origins))
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Include routers
+
 
 app.include_router(user_router)
 app.include_router(auth_router)

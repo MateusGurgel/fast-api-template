b9825b7da1fb45ea10db30964c7aac8cf7a02779
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-05-30 19:43:08 -0300

    refactor: update credential exception message for invalid login attempt

--- src/exceptions/credential_exception.py
+++ src/exceptions/credential_exception.py
@@ -2,4 +2,4 @@
 from src.exceptions.http_base_exception.http_exception import HttpException
 class credentialsException(HttpException):
     def __init__(self):
-        super().__init__(401, "Could not validate credentials")
+        super().__init__(401, "Invalid username or password. Please try again.")

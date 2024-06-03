d00e24095a17ef52847eaccbf9ffedf0a6df993c
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-06-02 23:52:18 -0300

    feat: email already exists implemented

---
+++ src/exceptions/email_already_used_exception.py
@@ -0,0 +1,5 @@
+
+from src.exceptions.http_base_exception.http_exception import HttpException
+class EmailAlreadyUsedException(HttpException):
+    def __init__(self):
+        super().__init__(409, "Email already in use.")
--- src/modules/user/user_service.py
+++ src/modules/user/user_service.py
@@ -1,7 +1,12 @@
+from src.exceptions.email_already_used_exception import EmailAlreadyUsedException
 from src.modules.user.user_schemas import CreateUserSchema
 from sqlalchemy.orm import Session
 from src.modules.user.user_repository import user_repository as repository
 
 
 def create_user(user: CreateUserSchema, db: Session) -> CreateUserSchema:
+
+    if repository.get_user_by_email(user.email, db):
+        raise EmailAlreadyUsedException()
+
     return repository.create_user(user, db)

5415216917a225c4dc6f43b8866a08ea426bf1f3
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-06-11 00:16:31 -0300

    fix: returning the right http status

--- src/exceptions/resource_not_found.py
+++ src/exceptions/resource_not_found.py
@@ -2,4 +2,4 @@
 from src.exceptions.http_base_exception.http_exception import HttpException
 class ResourceNotFound(HttpException):
     def __init__(self):
-        super().__init__(401, "resource not found")
+        super().__init__(404, "resource not found")

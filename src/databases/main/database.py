2c89387c1b37adcfdff48d5d1edce3a9c0ce2886
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-06-25 17:04:07 -0300

    fix: declarative_base deprecated warning removed

--- src/databases/main/database.py
+++ src/databases/main/database.py
@@ -1,8 +1,7 @@
 from decouple import config
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
